7c91f2e3051471cf7c430934bfab9e7614950f22
Michael Kiprotich <michael.kiprotich@student.moringaschool.com>
2022-01-17 13:01:28 +0300

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DatepipePipe } from './datepipe.pipe';
+import { HighlightDirective } from './highlight.directive';
+import { NavbarComponent } from './navbar/navbar.component';
+import { HomepageComponent } from './homepage/homepage.component';
+import { FooterComponent } from './footer/footer.component';
+import { SearchRepositoryComponent } from './search-repository/search-repository.component';
+import { SearchUserComponent } from './search-user/search-user.component';
+import { StarRepoDirective } from './Directives/star-repo.directive';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DatepipePipe,
+    HighlightDirective,
+    NavbarComponent,
+    HomepageComponent,
+    FooterComponent,
+    SearchRepositoryComponent,
+    SearchUserComponent,
+    StarRepoDirective
   ],
   imports: [
     BrowserModule,
