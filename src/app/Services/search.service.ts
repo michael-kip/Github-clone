5bf31bbb0bb772419ffbadbb2aea3968225a5adc
Michael Kiprotich <michael.kiprotich@student.moringaschool.com>
2022-01-17 17:37:40 +0300

    adding method to search user profile

--- src/app/Services/search.service.ts
+++ src/app/Services/search.service.ts
@@ -35,3 +35,12 @@ export class SearchService {
       );
   }
 }
+UpdateUser(username:string) {
+  this.username = username;
+}
+
+getUser() {
+ 
+  return this.http.get("https://api.github.com/users/" + this.username + "?client_id="  + "&client_secret=" );
+
+}
