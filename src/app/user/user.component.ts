54432db3676f433f1f3c60d65299f3912e8c0bf8
Michael Kiprotich <michael.kiprotich@student.moringaschool.com>
2022-01-17 14:28:43 +0300

    adding a function for finding user

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { SearchService } from '../search.service';
 
 @Component({
   selector: 'app-user',
@@ -6,8 +7,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./user.component.css']
 })
 export class UserComponent implements OnInit {
+  
+  public myGitName = 'michael-kip';
+  public githubUser!: string;
+    public users!:any;
+  user: any;
+
+  username: string = 'michael-kip'
+    
+  constructor(private myService:SearchService) { }
 
   constructor() { }
+
+  findUser () {
+    this.myService.UpdateUser(this.username);
+    this.myService.getUser().subscribe((user:any) => {
+      console.log(user);
+      this.user = user;
+    });
+
+  }
 
   ngOnInit(): void {
   }
