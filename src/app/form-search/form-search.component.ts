46082420a75f1d5326e5e5054eb876fef871c8be
Michael Kiprotich <michael.kiprotich@student.moringaschool.com>
2022-01-17 17:50:48 +0300

    improring search class

---
+++ src/app/form-search/form-search.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+import { GhSearch } from '../gh-search';
+
+@Component({
+  selector: 'app-form-search',
+  templateUrl: './form-search.component.html',
+  styleUrls: ['./form-search.component.css']
+})
+export class FormSearchComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
